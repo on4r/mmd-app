3e93321f6f0ffce6bc204f317f53140ead15343e
on4r <on4r@protonmail.com>
2020-01-09 10:40:35 -0300

    Update pwa config

--- vue.config.js
+++ vue.config.js
@@ -5,8 +5,6 @@ module.exports = {
   outputDir: process.env.OUTPUT_DIR,
   productionSourceMap: false,
   pwa: {
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: 'black',
     workboxPluginMode: 'GenerateSW',
     workboxOptions: {
       exclude: [/\.htaccess$/],
@@ -15,19 +13,19 @@ module.exports = {
       runtimeCaching: [
         {
           // Match any same-origin request that contains 'api'.
-          urlPattern: '/api/movies/',
+          urlPattern: /api\/movies/,
           // Apply a network-first strategy.
-          handler: 'NetworkFirst',
+          handler: 'StaleWhileRevalidate',
           options: {
             // Fall back to the cache after 10 seconds.
-            networkTimeoutSeconds: 10,
+            //networkTimeoutSeconds: 10,
             // Use a custom cache name for this route.
-            cacheName: 'my-api-cache',
+            //cacheName: 'my-api-cache',
             // Configure custom cache expiration.
-            expiration: {
-              maxEntries: 5,
-              maxAgeSeconds: 60,
-            },
+            //expiration: {
+            //  maxEntries: 5,
+            //  maxAgeSeconds: 60,
+            //},
             // Configure background sync.
             backgroundSync: {
               name: 'my-queue-name',
@@ -38,7 +36,6 @@ module.exports = {
             // Configure which responses are considered cacheable.
             cacheableResponse: {
               statuses: [0, 200],
-              headers: { 'x-test': 'true' },
             },
             // Configure the broadcast update plugin.
             broadcastUpdate: {
@@ -54,7 +51,7 @@ module.exports = {
             ],
             // matchOptions and fetchOptions are used to configure the handler.
             fetchOptions: {
-              mode: 'no-cors',
+              mode: 'same-origin',
             },
             matchOptions: {
               ignoreSearch: true,
@@ -72,6 +69,39 @@ module.exports = {
           }
         }
       ]
+    },
+    name: 'mmdb-app',
+    themeColor: '#ffdd57',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    assetsVersion: '',
+    manifestPath: 'manifest.json',
+    manifestOptions: {
+      name: 'mmdb-app', // pwa.name
+      short_name: 'mmdb', // pwa.name
+      start_url: '.',
+      display: 'standalone',
+      theme_color: '#ffdd57', // pwa.themeColor
+      icons: [
+        {
+          'src': './icons/icon-192x192.png',
+          'sizes': '192x192',
+          'type': 'image/png'
+        },
+        {
+          'src': './icons/icon-512x512.png',
+          'sizes': '512x512',
+          'type': 'image/png'
+        }
+      ]
+    },
+    iconPaths: {
+      favicon32: 'favicon/favicon-32x32.png',
+      favicon16: 'favicon/favicon-16x16.png',
+      appleTouchIcon: 'favicon/apple-touch-icon.png',
+      maskIcon: 'favicon/safari-pinned-tab.svg',
+      msTileImage: 'favicon/mstile-150x150.png'
     }
   }
 }
